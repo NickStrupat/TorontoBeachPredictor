c1047a9b08fad66871c5d1937fffa508256776f9
NickStrupat <nick.strupat@gmail.com>
2019-08-17 16:45:52 -0400

    fix formatting

--- Service/UnitOfLength.cs
+++ Service/UnitOfLength.cs
@@ -2,16 +2,16 @@
 
 namespace TorontoBeachPredictor.Service
 {
-    public class UnitOfLength
-    {
-        public static UnitOfLength Kilometers = new UnitOfLength(1.609344);
-        public static UnitOfLength NauticalMiles = new UnitOfLength(0.8684);
-        public static UnitOfLength Miles = new UnitOfLength(1);
+	public class UnitOfLength
+	{
+		public static UnitOfLength Kilometers = new UnitOfLength(1.609344);
+		public static UnitOfLength NauticalMiles = new UnitOfLength(0.8684);
+		public static UnitOfLength Miles = new UnitOfLength(1);
 
-        private readonly Double fromMilesFactor;
+		private readonly Double fromMilesFactor;
 
-        private UnitOfLength(Double fromMilesFactor) => this.fromMilesFactor = fromMilesFactor;
+		private UnitOfLength(Double fromMilesFactor) => this.fromMilesFactor = fromMilesFactor;
 
-        public Double ConvertFromMiles(Double input) => input * fromMilesFactor;
-    }
+		public Double ConvertFromMiles(Double input) => input * fromMilesFactor;
+	}
 }
